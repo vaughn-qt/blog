7b7980083f04b29d56b883f94cc9f995f9c13a43
Vaughn Andre Narvacan <vaughnandre@qualitytrade.com>
2023-03-22 08:34:28 +0800

    ---

--- database/migrations/2023_02_23_061403_create_comments_table.php
+++ database/migrations/2023_02_23_061403_create_comments_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
